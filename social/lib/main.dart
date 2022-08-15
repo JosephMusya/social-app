6804a6bb6c28aa0d8d2ab49a0b07f1c40c4ee256
JosephMusya <josephmusya254@gmail.com>
2022-08-15 14:43:05 +0300

    Added list display in listview builder

--- social/lib/main.dart
+++ social/lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_key_in_widget_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -24,17 +26,14 @@ class MainBody extends StatefulWidget {
 }
 
 class _MainBodyState extends State<MainBody> {
-  final controller = TextEditingController();
   String text = '';
   List<Post> posts = [];
 
-  void submit() {
+  void submit(text) {
     setState(() {
-      text = controller.text;
-      posts.add(Post(text, 'Joe', 0, 'Joe'));
+      this.text = text;
+      posts.add(Post(text, 'Joe', 0, false));
     });
-
-    controller.clear();
   }
 
   @override
@@ -44,48 +43,50 @@ class _MainBodyState extends State<MainBody> {
           title: const Center(child: Text('Social App')),
         ),
         body: Column(children: [
-          Expanded(
-              child: ListView.builder(
-                  itemCount: posts.length,
-                  itemBuilder: (context, index) {
-                    var post = posts[index];
-                    print(post.body);
-                    return Card(
-                        child: Row(
-                      children: [
-                        Expanded(
-                            child: ListTile(
-                          title: Text(post.body),
-                          subtitle: Text(post.author),
-                        )),
-                        Row(
-                          children: [
-                            IconButton(
-                                onPressed: post.likePost,
-                                icon: const Icon(Icons.thumb_up)),
-                          ],
-                        )
-                      ],
-                    ));
-                  })),
+          Expanded(child: PostView(posts)),
           const SizedBox(width: 50),
-          Expanded(
-            child: Container(
-                padding: const EdgeInsets.all(18.0),
-                child: TextField(
-                  controller: controller,
-                  decoration: InputDecoration(
-                    prefixIcon: const Icon(Icons.message),
-                    suffixIcon: IconButton(
-                      onPressed: submit,
-                      icon: const Icon(Icons.send),
-                      tooltip: 'Post Message',
-                    ),
-                    labelText: 'Type Message',
-                  ),
-                )),
-          ),
+          Expanded(child: TextInput(submit))
         ]));
+  }
+}
+
+class TextInput extends StatefulWidget {
+  final Function callback;
+  const TextInput(this.callback);
+
+  @override
+  State<TextInput> createState() => _TextInputState();
+}
+
+class _TextInputState extends State<TextInput> {
+  final controller = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
+  void click() {
+    widget.callback(controller.text);
+    print("Submited....");
+    controller.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+        prefixIcon: const Icon(Icons.message),
+        suffixIcon: IconButton(
+          onPressed: click,
+          icon: const Icon(Icons.send),
+          tooltip: 'Post Message',
+        ),
+        labelText: 'Type Message',
+      ),
+    );
   }
 }
 
@@ -95,7 +96,7 @@ class Post {
   int likes = 0;
   bool userLiked = false;
 
-  Post(body, author, likes, userLiked);
+  Post(this.body, this.author, this.likes, this.userLiked);
 
   void likePost() {
     if (!userLiked) {
@@ -104,5 +105,41 @@ class Post {
     }
     userLiked = false;
     likes -= 1;
+  }
+}
+
+class PostView extends StatefulWidget {
+  final List<Post> listitems;
+  const PostView(this.listitems);
+  @override
+  State<PostView> createState() => _PostViewState();
+}
+
+class _PostViewState extends State<PostView> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: widget.listitems.length,
+        itemBuilder: (context, index) {
+          var post = widget.listitems[index];
+          return Card(
+            child: Row(
+              children: [
+                Expanded(
+                    child: ListTile(
+                  title: Text(post.body),
+                  subtitle: Text(post.author),
+                )),
+                Row(
+                  children: [
+                    IconButton(
+                        onPressed: post.likePost,
+                        icon: const Icon(Icons.thumb_up)),
+                  ],
+                ),
+              ],
+            ),
+          );
+        });
   }
 }
