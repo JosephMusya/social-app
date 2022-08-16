d87e304dc7d05a32f58145b019101f90c2ded323
JosephMusya <josephmusya254@gmail.com>
2022-08-16 19:46:56 +0300

    Added the toggle button

--- social/lib/views/homepage.dart
+++ social/lib/views/homepage.dart
@@ -15,6 +15,18 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   String text = '';
   List<Post> posts = [];
+  bool show = false;
+
+  void toggle() {
+    if (!show) {
+      show = true;
+    } else if (show) {
+      show = false;
+    }
+    setState(() {
+      show;
+    });
+  }
 
   void submit(text) {
     setState(() {
@@ -25,14 +37,30 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    bool showAdd = MediaQuery.of(context).viewInsets.bottom != 0;
+
     return Scaffold(
         appBar: AppBar(
+          actions: [
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Visibility(
+                visible: (!showAdd),
+                child: IconButton(
+                  onPressed: toggle,
+                  icon: show
+                      ? const Icon(Icons.minimize_sharp)
+                      : const Icon(Icons.add),
+                ),
+              ),
+            )
+          ],
           title: const Text("Social App"),
         ),
         body: Column(
           children: [
             Expanded(child: PostView(posts)),
-            TextInput(submit),
+            Visibility(visible: (show), child: TextInput(submit)),
           ],
         ));
   }
